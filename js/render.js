e503e5b8812a3f7368cf18e11e596fbd0858bbf7
Stepan <108026663+stepan95@users.noreply.github.com>
2023-08-16 10:48:31 +0300

    Update render.js

--- js/render.js
+++ js/render.js
@@ -235,7 +235,7 @@ document.addEventListener('touchmove', (event) => {
     const newY = event.touches[0].clientY;
     const deltaY = newY - touchY;
 
-    butterfly.y += deltaY*4; // Зміщуємо метелика по вертикалі
+    butterfly.y += deltaY*3; // Зміщуємо метелика по вертикалі
     touchY = newY; // Оновлюємо вертикальну координату дотику
 
     if (deltaY > 0) {
@@ -254,7 +254,7 @@ document.addEventListener('touchmove', (event) => {
     const newX = event.touches[0].clientX;
     const deltaX = newX - touchX;
 
-    butterfly.x += deltaX; // Зміщуємо метелика по горизонталі
+    butterfly.x += deltaX*3; // Зміщуємо метелика по горизонталі
     touchX = newX; // Оновлюємо вертикальну координату дотику
   }
 });
